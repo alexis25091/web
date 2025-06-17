159e71351ae394dea2216974c44ec8de3aba533a
Alan Alexis Suarez Moreno <152936639+alexis25091@users.noreply.github.com>
2025-06-16 23:51:36 -0600

    se modifico el conexion para usar PDO

--- script/conexion.php
+++ script/conexion.php
@@ -1,15 +1,28 @@
 <?php
-    $serverName = "DESKTOP-TI91AFN\\SQLEXPRESS"; // Nombre correcto de tu servidor
-    $connectionOptions = [
-        "Database" => "music_and_talent",
-        "TrustServerCertificate" => true, // Evita errores de certificados
-        "CharacterSet" => "UTF-8"         // Codificación
-    ];
 
-    // Conexión con autenticación de Windows
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
+class conexion{
+    public static function conexionBD(){
 
-    if ($conn === false) {
-        die(print_r(sqlsrv_errors(), true));
+        $host="localhost";
+        $dbname="music_and_talent";
+        $username="alexisbases";
+        $password="alexisbases";
+        $port="1433";
+
+        try{
+
+            $conn= new PDO("sqlsrv:server=$host,$port;Database=$dbname",$username,$password);
+            $conn -> setAttribute(PDO:: ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            echo ("se conecto a la base de datos");
+        }
+        catch(PDOException $pe){
+
+            die("no se logro conectar a la base de datos".$pe -> getMessage());
+        }
+
+        return $conn;
     }
+}
+
+
 ?>
