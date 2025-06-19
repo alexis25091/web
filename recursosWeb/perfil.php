629737cfb3881dfccc76e13bc8e896b37ccdf87f
Alan Alexis Suarez Moreno <152936639+alexis25091@users.noreply.github.com>
2025-06-19 14:36:50 -0600

    Add files via upload

---
+++ recursosWeb/perfil.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="../estilos/template.css">
+    <link rel="stylesheet" href="../estilos/perfil.css">
+
+</head>
+<body>
+
+<!-- Header -->
+    <header class="navbar">
+        <a href="../index.html">
+            <img src="../imagenes/logo.png" alt="Logo de la empresa" class="logo">
+        </a>
+        <nav class="nav-links">
+            <a href="nosotros.html">Nosotros</a>
+            <a href="servicios.html">Servicios</a>
+            <a href="publicarEmpleo.html">Publicar Empleo</a>
+            <a href="mensajes.php">Mensajes</a>
+            <a href="iniciarSesion.html">Ingresar</a>
+            <a href="perfil.php" class="profile-icon" title="Ver perfil">
+                <img src="../imagenes/usuario.png" alt="usuario">
+            </a>
+        </nav>
+    </header>
+
+
+    <!-- Pantalla principal (va a cambiar) -->
+    <main class="contenedor_opciones">
+
+        <section>
+            <h1 class="titulo_perfil">Perfil</h1>
+        <a href="./informacionPersonal.html" class="boton_opciones">
+            <h2>Información personal</h2>
+        </a>
+        <a href="./experiencia.html" class="boton_opciones">
+            <h2>Experiencia</h2>
+        </a>
+        <a href="./habilidades.html" class="boton_opciones">
+            <h2>Habilidades</h2>
+        </a>
+        <a href="./configuracion.html" class="boton_opciones">
+            <h2>Configuración</h2>  
+        </a>
+        </section>
+
+    </main>
+
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="footer-left">
+            <a href="../index.html">
+                <img src="../imagenes/logo.png" alt="Logo de la empresa" class="logo-footer">
+            </a>
+        </div>
+        <div class="footer-right">
+            <a href="#">Términos y condiciones</a>
+            <a href="#">Políticas</a>
+        </div>
+    </footer>
+
+</body>
+
+</html>
